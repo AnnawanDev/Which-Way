751562b29eef5a61c5c74fe4f1b8ec671466084c
Ed Wied <wied.ed@gmail.com>
2020-09-12 14:52:03 -0500

    add app icons, set up basics for distance determination, set up AR screen

--- Which-Way/Models/Holes.swift
+++ Which-Way/Models/Holes.swift
@@ -16,7 +16,7 @@ class Holes: NSObject, UIPickerViewDataSource {
     var holeChoices: [String] = ["Hole 1", "Hole 2", "Hole 3", "Hole 4", "Hole 5", "Hole 6", "Hole 7", "Hole 8", "Hole 9", "Hole 10", "Hole 11", "Hole 12", "Hole 13", "Hole 14", "Hole 15", "Hole 16", "Hole 17", "Hole 18"]
     
     //set initial GPS coordinates of holes to GPS
-    var holeGPSLocations:[(long:Double, lat:Double)] = [(0.0, 0.0), (0.0, 0.0), (0.0, 0.0), (0.0, 0.0), (0.0, 0.0), (0.0, 0.0), (0.0, 0.0), (0.0, 0.0), (0.0, 0.0), (0.0, 0.0), (0.0, 0.0), (0.0, 0.0), (0.0, 0.0), (0.0, 0.0), (0.0, 0.0), (0.0, 0.0), (0.0, 0.0), (0.0, 0.0)]
+    var holeGPSLocations:[(long:Double, lat:Double)] = [(-89.9082712, 41.3974428), (0.0, 0.0), (0.0, 0.0), (0.0, 0.0), (0.0, 0.0), (0.0, 0.0), (0.0, 0.0), (0.0, 0.0), (0.0, 0.0), (0.0, 0.0), (0.0, 0.0), (0.0, 0.0), (0.0, 0.0), (0.0, 0.0), (0.0, 0.0), (0.0, 0.0), (0.0, 0.0), (0.0, 0.0)]
     
     //implementing UIPickerViewDataSource required delegate method
     func numberOfComponents(in: UIPickerView) -> Int {
--- Which-Way/ViewControllers/ARViewController.swift
+++ Which-Way/ViewControllers/ARViewController.swift
@@ -8,13 +8,45 @@
 
 import UIKit
 import ARKit
+import CoreLocation
 
-class ARViewController: BaseViewController {
+
+class ARViewController: BaseViewController, CLLocationManagerDelegate {
+    
+    //outlets
+    @IBOutlet weak var distanceAwayInYards: UILabel!
+    @IBOutlet weak var sceneView: ARSCNView!
+    
+    //instance variables
+    var locationManager: CLLocationManager!
+    var userCurrentLongitude: Double = 0.0
+    var userCurrentLatitude: Double = 0.0
+    var holes:Holes!
+    let configruation = ARWorldTrackingConfiguration()
+    
+    //overrides
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // initialize hole data
+        holes = Holes.holeDS
+        
+        //add AR debug options
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+        
+        // as soon as scene view, need to mnake sure scene view has configuration
+        self.sceneView.session.run(configruation)
+        
+        //make sure distance label is on top
+        self.distanceAwayInYards.layer.zPosition = 100
+
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        determineUserLocation()
     }
 
+    //buttons
     @IBAction func press_home(_ sender: Any) {
         transition(goingTo: Destination.homeID.rawValue)
     }
@@ -23,4 +55,50 @@ class ARViewController: BaseViewController {
         transition(goingTo: Destination.courseID.rawValue)
     }
     
+    // delegate methods
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let userCurrentLocation:CLLocation = locations[0] as CLLocation
+        userCurrentLatitude = userCurrentLocation.coordinate.latitude
+        userCurrentLongitude = userCurrentLocation.coordinate.longitude
+        getDistance()
+    }
+    
+    //helper functions
+    func determineUserLocation() {
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+        
+        if !CLLocationManager.locationServicesEnabled() {
+            print("You must enabled location services")
+        } else {
+            locationManager.startUpdatingLocation()
+        }
+    }
+    
+    func getDistance() {
+        //https://developer.apple.com/documentation/corelocation/cllocation/1423689-distance
+        //distance(from:)
+        //Returns the distance (measured in meters) from the current object's location to the specified location.
+        //func distance(from location: CLLocation) -> CLLocationDistance
+        
+        let holeLocation = CLLocation(latitude:holes.holeGPSLocations[0].lat, longitude: holes.holeGPSLocations[0].long)
+        let secondLocation = CLLocation(latitude: userCurrentLatitude, longitude: userCurrentLongitude)
+        let distance = holeLocation.distance(from: secondLocation) //distance in meters
+        let distanceInYards = distance * 1.0936
+        let doubleFormat = ".1"
+        print("user latitude: \(userCurrentLatitude)")
+        print("user longitude:  \(userCurrentLongitude)")
+        print(" \(String(format:"%.02f", distanceInYards)) yards ")
+        print("-----------------------------")
+        distanceAwayInYards.text = "Distance to pin: \(distanceInYards.format(f: doubleFormat))"
+    }
+}
+
+//extension method used from https://stackoverflow.com/questions/24051314/precision-string-format-specifier-in-swift
+extension Double {
+    func format(f: String) -> String {
+        return String(format: "%\(f)f", self)
+    }
 }

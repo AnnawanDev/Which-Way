08308ffdcf67c52b1fb8768085a9c12ae58ba7db
Ed Wied <wied.ed@gmail.com>
2020-09-13 13:48:51 -0500

    adjusting layout

--- Which-Way/ViewControllers/AboutViewController.swift
+++ Which-Way/ViewControllers/AboutViewController.swift
@@ -21,7 +21,7 @@ class AboutViewController: BaseViewController {
 
         Hacathon page on DevPost: https://beaverhacks-fall-2020.devpost.com/
         
-        The focus on the hackathon is "health" which I took as promoting healthy lifestyles by encouraging fitness. This app addresses the issue of not being able to find the golf flag (or pin) while on a course. Sometimes there are hills, trees, or other obstacls that block view of exactly where the pin is located. This helps the golfer orient toward the flag.
+        This app addresses the issue of not being able to find the golf flag (or pin) while on a course. Sometimes there are hills, trees, or other obstacls that block view of exactly where the pin is located. This helps the golfer orient toward the flag.  The app makes use of Core Location to get the location of the user and the destination and then ARKit to serve visual markers on where the golfer should orient.
 
         This app was made by Ed Wied, a fair amount of coffee, and his trusty orange cat Starbuck
 
